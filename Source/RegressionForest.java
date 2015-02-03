4be9b842affc438789de394ba833d73c6b29f459
Kim Hansson <kim.hansson@live.se>
2015-02-03 14:38:08 +0100

    more implementations on supervised forest, reading for testing

--- Source/RegressionForest.java
+++ Source/RegressionForest.java
@@ -4,20 +4,31 @@ import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
 import weka.classifiers.trees.RandomForest;
+import weka.classifiers.Evaluation;
 public abstract class RegressionForest 
 {
 	protected int m_maxDepth, m_numTrees, m_features;
 	protected ArffLoader m_loader;
 	protected Instances m_structure;
 	protected RandomForest m_forest;
+	protected Evaluation m_evaluator;
 	public RegressionForest(int p_maxDepth, int p_numTrees, int p_features)
 	{
-	
+		m_maxDepth = p_maxDepth;
+		m_numTrees = p_numTrees;
+		m_features = p_features;
+		
+		m_forest = new RandomForest();
 	}
 	
-	abstract void Train(String p_trainingData);
+	public String GetResult()
+	{
+		return "hai";
+	}
 	
-	abstract void Run(String p_data);
+	abstract void Train(String p_data) throws Exception; 
+	
+	abstract double[] Run(String p_data) throws Exception;
 	
 	protected void ReadFile(String p_file) throws Exception
 	{
@@ -27,11 +38,9 @@ public abstract class RegressionForest
 		
 		m_structure.setClassIndex(m_structure.numAttributes() - 1);
 		
+		m_evaluator = new Evaluation(m_structure);
 	}
 	
-	public String GetResult()
-	{
-		return "hai";
-	}
+	
 	
 }
--- Source/SupervisedForest.java
+++ Source/SupervisedForest.java
@@ -1,19 +1,35 @@
+import java.io.File;
+import java.util.Iterator;
+import java.util.Random;
 
 public class SupervisedForest extends RegressionForest {
 
 	public SupervisedForest(int p_maxDepth, int p_numTrees, int p_features) {
 		super(p_maxDepth, p_numTrees, p_features);
-		// TODO Auto-generated constructor stub
 	}
 
-	void Train(String p_trainingData) {
-		// TODO Auto-generated method stub
+	void Train(String p_data) throws Exception {
 		
+		ReadFile(p_data);
+		
+		String[] Options = new String[1];
+		
+		m_evaluator.crossValidateModel(m_forest, m_structure, 10, new Random(1), Options);
+
 	}
 
-	void Run(String p_data) {
-		// TODO Auto-generated method stub
+	double[] Run(String p_data) throws Exception {
 		
+		ReadFile(p_data);
+		
+		double[] returnValue = new double[m_structure.numInstances()];
+		
+		for(int i = 0; i < m_structure.numInstances(); i++)
+		{
+			returnValue[i] = m_forest.classifyInstance(m_structure.get(i));
+		}
+		
+		return returnValue;
 	}
 
 
