df81a27b417c58ab8e6cd55c1d26c4e3d0357029
Kim Hansson <kim.hansson@live.se>
2015-02-10 15:16:02 +0100

    regression forest now extends random forest from weka

--- Source/RegressionForest.java
+++ Source/RegressionForest.java
@@ -5,7 +5,7 @@ import weka.core.Instances;
 import weka.classifiers.trees.RandomForest;
 import weka.classifiers.Evaluation;
 
-public abstract class RegressionForest 
+public abstract class RegressionForest extends weka.classifiers.trees.RandomForest
 {
 	protected int m_maxDepth, m_numTrees, m_features;
 	protected Instances m_structure;
@@ -18,7 +18,7 @@ public abstract class RegressionForest
 		m_features = p_features;
 		
 		m_forest = new RandomForest();
-		
+		m_forest.setNumExecutionSlots(8);
 	}
 	
 	abstract void SetData(Instances p_data) throws Exception;
